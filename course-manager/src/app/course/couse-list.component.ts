b08af5a29fa531a23c413ca2c7a74b24264cebca
WashingtonLuiz89 <wl-cg@hotmail.com>
2020-11-30 06:30:00 -0300

    update

---
+++ course-manager/src/app/course/couse-list.component.ts
@@ -0,0 +1,13 @@
+import { Template } from '@angular/compiler/src/render3/r3_ast';
+import { Component } from '@angular/core' ;
+
+@Component({
+selector: 'app-course-list',
+template: '<h2> Course List </h2>'
+})
+
+
+export class CourseListComponent{
+
+
+}
